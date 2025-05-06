17b452327175ba7e183f2f68de635e34a2f49565
yurdanuryigitege <75130781+yurdanuryigitege@users.noreply.github.com>
2025-05-06 15:49:39 +0300

    Update yazdır.py

--- yazdır.py
+++ yazdır.py
@@ -1,11 +1,14 @@
 import pandas as pd
 
-# Mahalle verilerini içeren sözlük
+# PTT şube verilerini içeren sözlük
 data = {
-    "Mahalle Adı": ["Muttalip Orta Mah.", "Çanakkıran Mah.", "Yukarıılıca Mah.", 
-                    "Karacaşehir Mah.", "Akpınar Mah.", "Aşağı Çağlan Mah.", "Ayvacık Mah."],
-    "Enlem": [39.839491, 39.669940, 39.537610, 39.737778, 39.659440, 39.678720, 39.536200],
-    "Boylam": [30.546304, 30.250660, 30.447030, 30.456667, 30.547780, 30.483750, 30.484490]
+    "Şube Adı": ["Odunpazarı PTT Merkez", "Emek Mahallesi Şubesi", "Gar Merkezi Şubesi", 
+                  "Orhangazi Şubesi", "Sakarya Şubesi", "Çukurhisar Şubesi", "Batıkent Şubesi", 
+                  "71 Evler Şubesi", "Kırmızıtoprak Şubesi", "Mamure Şubesi"],
+    "Enlem": [39.766930, 39.766970, 39.775200, 39.770000, 39.776890, 39.723010, 39.770220, 
+              39.780490, 39.775800, 39.776120],
+    "Boylam": [30.519280, 30.520580, 30.528330, 30.517530, 30.520570, 30.501340, 30.528700, 
+               30.524000, 30.529100, 30.501430]
 }
 
 # DataFrame oluştur
